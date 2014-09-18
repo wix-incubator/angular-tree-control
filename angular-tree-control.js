3ecb04df872b0f2a8ffb089185507412ded089cf
Carlos Gomes <carlos@mojotech.com>
2014-09-18 14:39:41 -0400

    whitespace cleanup

--- angular-tree-control.js
+++ angular-tree-control.js
@@ -1,6 +1,6 @@
 (function ( angular ) {
     'use strict';
-    
+
     angular.module( 'treeControl', [] )
         .directive( 'treecontrol', ['$compile', function( $compile ) {
             /**
@@ -17,12 +17,12 @@
                 else
                     return "";
             }
-            
+
             function ensureDefault(obj, prop, value) {
                 if (!obj.hasOwnProperty(prop))
                     obj[prop] = value;
             }
-            
+
             return {
                 restrict: 'EA',
                 require: "treecontrol",
