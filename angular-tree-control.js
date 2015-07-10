45d6f70f54caa4559584f2b017888d465b277880
Stuart McGrigor <stumcg@aviarc.com>
2015-07-10 16:36:19 +1200

    Actual ContextMenu stuff is now it's own compoent, attribute menu-id tells treecontrol to use it

--- angular-tree-control.js
+++ angular-tree-control.js
@@ -1,19 +1,6 @@
 (function ( angular ) {
     'use strict';
 
-    /* Figure out page (viewport) dimensions of current page, by
-     * putting an empty DIV in the bottom right, and checking its offset.
-     */
-    function getPageDimensions() {
-      var bttmRight = document.createElement("div");
-      bttmRight.setAttribute("style" , "visibility:hidden;position:fixed;bottom:0px;right:0px;");
-      document.getElementsByTagName("body")[0].appendChild(bttmRight);
-      var pageWidth = bttmRight.offsetLeft;
-      var pageHeight = bttmRight.offsetTop;
-      bttmRight.parentNode.removeChild(bttmRight);
-      return { pageWidth:pageWidth, pageHeight:pageHeight };
-    };
-
     angular.module( 'treeControl', [] )
         .directive( 'treecontrol', ['$compile', function( $compile ) {
             /**
@@ -48,9 +35,10 @@
                     onSelection: "&",
                     onNodeToggle: "&",
                     onRightClick: "&",
-                    contextMenuId: "@",
+                    menuId: "@",
                     options: "=?",
                     orderBy: "@",
+                    orderByExpression: "@",
                     reverseOrder: "@",
                     filterExpression: "=?",
                     filterComparator: "=?"
@@ -230,9 +218,14 @@
                     };
 
                     //tree template
-                    var orderBy = $scope.orderBy ? ' | orderBy:orderBy:reverseOrder' : '';
-                    var rcLabel = $attrs.onRightClick ? ' tree-right-click="rightClickNodeLabel(node)"' : '';
-                    var ctxMenuId = $attrs.contextMenuId ? ' tree-context-menu-id="'+ $attrs.contextMenuId+'"' : '';
+                    var orderBy = '';
+                    if ($scope.orderByExpression) {
+                        orderBy = ' | orderBy:' + $scope.orderByExpression + ':reverseOrder';
+                    } else if ($scope.orderBy) {
+                        orderBy = ' | orderBy:orderBy:reverseOrder';
+                    }
+                    var rcLabel = $scope.onRightClick ? ' tree-right-click="rightClickNodeLabel(node)"' : '';
+                    var ctxMenuId = $scope.menuId ? ' context-menu-id="'+ $scope.menuId+'"' : '';
 
                     var template =
                         '<ul '+classIfDefined($scope.options.injectClasses.ul, true)+'>' +
@@ -250,7 +243,6 @@
 
                     this.template = $compile(template);
                 }],
-
 
                 compile: function(element, attrs, childTranscludeFn) {
                     return function ( scope, element, attrs, treemodelCntr ) {
@@ -328,130 +320,6 @@
             };
         })
 
-        .directive('treeContextMenuId', ['$document', function($document) {
-
-            return {
-                restrict    : 'A',
-                scope       : '@&',
-                compile: function compile(tElement, tAttrs, transclude) {
-                    return {
-                        post: function postLink(scope, iElement, iAttrs, controller) {
-
-                            var ul = angular.element(document.querySelector('#' + iAttrs.treeContextMenuId));
-
-                            ul.css({ 'display' : 'none'});
-
-                            // right-click on context-menu will show the menu
-                            iElement.bind('contextmenu', function showContextMenu(event) {
-
-                                // don't do the normal browser right-click context menu
-                                event.preventDefault();
-                                var pgDim = getPageDimensions();
-
-                                // will ctxMenu fit on screen (height-wise) ?
-                                // TODO: figure out why we need the fudge-factor of 14
-                                var ulTop = event.clientY + ul.height() <= pgDim.pageHeight - 14
-                                          ? event.clientY
-                                          : pgDim.pageHeight - ul.height() - 14;
-
-                                // will ctxMenu fit on screen (width-wise) ?
-                                var ulLeft = event.clientX + ul.width() <= pgDim.pageWidth - 2
-                                           ? event.clientX
-                                           : pgDim.pageWidth - ul.width() - 2;
-
-                                // use CSS to move and show the dropdown-menu
-                                ul.css({
-                                    position: "fixed",
-                                    display: "block",
-                                    left: ulLeft + 'px',
-                                    top:  ulTop  + 'px'
-                                });
-
-                                // setup a one-time click event on the document to hide the dropdown-menu
-                                $document.one('click', function hideContextMenu(event) {
-                                    ul.css({
-                                        'display' : 'none'
-                                    });
-                                });
-                            });
-                        }
-                    };
-                }
-            };
-        }])
-
-        .directive('contextSubmenuId', ['$document', function($document) {
-            return {
-                restrict    : 'A',
-                scope       : '@&',
-                compile: function compile(tElement, tAttrs, transclude) {
-                    return {
-                        post: function postLink(scope, iElement, iAttrs, controller) {
-
-                            var ul = angular.element(document.querySelector('#' + iAttrs.contextSubmenuId));
-
-                            ul.css({ 'display' : 'none'});
-
-
-                            iElement.bind('mouseover', function showSubContextMenu(event) {
-                                // use CSS to move and show the sub dropdown-menu
-                                if(ul.css("display") == 'none') {
-
-                                  // Will the ctxMenu fit on the right of the parent menu ??
-                                  var pgDim = getPageDimensions();
-
-                                  // will ctxMenu (height-wise) ?
-                                  // TODO: figure out why we need the fudge-factor of 14
-                                  var ulTop = event.clientY + ul.height() <= pgDim.pageHeight - 14
-                                            ? event.clientY
-                                            : pgDim.pageHeight - ul.height() - 14;
-
-                                  // will ctxMenu (width-wise) ?
-                                  var ulLeft =
-                                    (event.target.offsetParent.offsetLeft +
-                                     event.target.clientWidth +
-                                     ul.width() < pgDim.pageWidth) ? event.target.offsetParent.offsetLeft +
-                                                                     event.target.clientWidth
-
-                                                                    : event.target.offsetParent.offsetLeft -
-                                                                      ul.width();
-
-                                    ul.css({
-                                        position: "fixed",
-                                        display: "block",
-                                        left: ulLeft + 'px',
-                                        top:  ulTop + 'px'
-                                    })
-
-                                    // Each uncle/aunt menu item needs a mouseover event to make the subContext menu disappear
-                                    angular.forEach(iElement[0].parentElement.parentElement.children, function(child, ndx) {
-                                        if(child !== iElement[0].parentElement) {
-                                            angular.element(child).one('mouseover', function(event) {
-                                                if(ul.css("display") == 'block') {
-                                                    ul.css({
-                                                        'display' : 'none'
-                                                    });
-                                                }
-                                            });
-                                        }
-                                    });
-                                }
-
-                                // setup a one-time click event on the document to hide the sub dropdown-menu
-                                $document.one('click', function hideContextMenu(event) {
-                                    if(ul.css("display") == 'block') {
-                                        ul.css({
-                                            'display' : 'none'
-                                        });
-                                    }
-                                });
-                            });
-                        }
-                    };
-                }
-            };
-        }])
-
         .directive("treeitem", function() {
             return {
                 restrict: 'E',
@@ -464,6 +332,7 @@
                 }
             };
         })
+
         .directive("treeTransclude", function() {
             return {
                 link: function(scope, element, attrs, controller) {
