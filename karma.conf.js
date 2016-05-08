adc9af059811216e06c62e780629e99d29177119
destromas1 <shahjada.talukdar@gmail.com>
2016-05-08 14:46:05 +0600

    jQuery Bower location fix in Grunt and Karma file

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'bower_components/jquery/jquery.js',
+      'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'angular-tree-control.js',
